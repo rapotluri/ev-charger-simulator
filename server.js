4ab9e0e2a601e3d3a0bd294a085d2eafad61c821
Rudra Aryan Potluri <67205254+rapotluri@users.noreply.github.com>
2023-12-20 21:57:58 -0800

    Added StartTransaction & StopTransaction

--- server.js
+++ server.js
@@ -62,17 +62,80 @@ class OcppChargerSimulator {
         console.log(`StatusNotification sent from charger ${this.chargerId} with status ${status}`);
     }
 
+    startTransaction(idTag) {
+        const startTransactionPayload = {
+          connectorId: 1, // Assuming only one connector for the simulation
+          idTag: idTag,
+          meterStart: 0,
+          timestamp: new Date().toISOString(),
+        };
+        const message = JSON.stringify([2, this.generateUniqueId(), 'StartTransaction', startTransactionPayload]);
+        this.websocket.send(message);
+        console.log(`StartTransaction sent from charger ${this.chargerId} with idTag ${idTag}`);
+    }
+    
+    stopTransaction(transactionId) {
+        const stopTransactionPayload = {
+          transactionId: transactionId,
+          idTag: "SampleTag", // Replace with actual tag if needed
+          meterStop: 1337, // Replace with the actual meter value
+          timestamp: new Date().toISOString(),
+          reason: 'EVDisconnected'
+        };
+        const message = JSON.stringify([2, this.generateUniqueId(), 'StopTransaction', stopTransactionPayload]);
+        this.websocket.send(message);
+        console.log(`StopTransaction sent from charger ${this.chargerId} for transactionId ${transactionId}`);
+    }
+
     generateUniqueId() {
         return `msg${Math.random().toString(16).substr(2, 8)}`;
     }
 }
 
+const chargers = {};
+
 app.get('/start-charger/:chargerId', (req, res) => {
     const chargerId = req.params.chargerId;
+
+    if (chargers[chargerId]) {
+        return res.status(400).send('Charger already exists');
+    }
+
+
     const centralSystemUrl = 'ws://centralsystem.hypercharge.com/ocpp'; // Replace with your central system URL
     const heartbeatInterval = 10000; // Heartbeat interval in milliseconds (e.g., 10000 for 10 seconds)
-    new OcppChargerSimulator(chargerId, centralSystemUrl, heartbeatInterval);
+    const charger = new OcppChargerSimulator(chargerId, centralSystemUrl, heartbeatInterval);
+
+    chargers[chargerId] = charger; // Store the charger instance
+
     res.send(`Started charger simulation for charger ID: ${chargerId}`);
+});
+
+
+// Endpoint to simulate starting a transaction
+app.get('/chargers/:chargerId/start-transaction', (req, res) => {
+    const chargerId = req.params.chargerId;
+    const idTag = req.query.idTag; // Expect an idTag as query parameter
+  
+    if (!chargers[chargerId]) {
+      return res.status(404).send('Charger not found');
+    }
+  
+    chargers[chargerId].startTransaction(idTag);
+    res.send(`Transaction started for charger ID: ${chargerId} with idTag: ${idTag}`);
+});
+  
+// Endpoint to simulate stopping a transaction
+app.get('/chargers/:chargerId/stop-transaction', (req, res) => {
+    const chargerId = req.params.chargerId;
+    const transactionId = req.query.transactionId; // Expect a transactionId as query parameter
+  
+    if (!chargers[chargerId]) {
+      return res.status(404).send('Charger not found');
+    }
+  
+    chargers[chargerId].stopTransaction(transactionId);
+    res.send(`Transaction stopped for charger ID: ${chargerId} with transactionId: ${transactionId}`);
 });
 
 app.listen(port, () => {
